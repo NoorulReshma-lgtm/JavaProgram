6f43d42d9f71b5dfdd9f1391d3084cb1f6ef6ad9
NoorulReshma-lgtm <sabi84reshma@gmail.com>
2026-07-23 18:00:57 +0400

    Create 17_AllPairsWithGivenSum.java

---
+++ 17_AllPairsWithGivenSum.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class AllPairsWithGivenSum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the target sum: ");
+        int target = sc.nextInt();
+
+        boolean found = false;
+
+        System.out.println("Pairs with Sum " + target + ":");
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No pairs found.");
+        }
+
+        sc.close();
+    }
+}
