ce8f91fc660f77431ed77887cd6a5f2c46b941aa
NoorulReshma-lgtm <sabi84reshma@gmail.com>
2026-07-23 18:01:49 +0400

    Create 18_TripletsSumZero.java

---
+++ 18_TripletsSumZero.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class TripletsSumZero {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean found = false;
+
+        System.out.println("Triplets with Sum Zero:");
+
+        for (int i = 0; i < n - 2; i++) {
+
+            for (int j = i + 1; j < n - 1; j++) {
+
+                for (int k = j + 1; k < n; k++) {
+
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        System.out.println("(" + arr[i] + ", " + arr[j] + ", " + arr[k] + ")");
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No triplets found with sum zero.");
+        }
+
+        sc.close();
+    }
+}
